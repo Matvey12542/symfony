3e3c6765d6ce7cc726d54e262262e95e251f891c
kolya <kolya@kolya-RV410-RV510-S3510-E3510.(none)>
2014-06-11 23:32:52 +0300

    add data fixtures and migrations

--- symfony2blog/src/Acme/StoreBundle/DataFixtures/ORM/LoadProduct.php
+++ symfony2blog/src/Acme/StoreBundle/DataFixtures/ORM/LoadProduct.php
@@ -12,13 +12,29 @@ use Doctrine\Common\Persistence\ObjectManager;
 use Acme\StoreBundle\Entity\Product;
 
 class LoadProductData implements FixtureInterface {
+    public function getOrder() {
+        return 10;
+    }
+
     public function load(ObjectManager $manager){
         $product = new Product();
         $product -> setName('Product1');
         $product -> setPrice(18);
         $product->setDescription('Lorem ipsum sefis oeioseh fioso sofe osie');
 
+        $product2 = new Product();
+        $product2 -> setName('Product2');
+        $product2 -> setPrice(18);
+        $product2->setDescription('Lorem2222 ipsum sefis oeioseh fioso sofe osie');
+
+        $product3 = new Product();
+        $product3 -> setName('Product3');
+        $product3 -> setPrice(18);
+        $product3->setDescription('Lorem3333 ipsum sefis oeioseh fioso sofe osie');
+
         $manager->persist($product);
+        $manager->persist($product2);
+        $manager->persist($product3);
         $manager->flush();
     }
 } 
